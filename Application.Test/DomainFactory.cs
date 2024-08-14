69a77184d596148b893c73ec994abf814d824c21
Ludwig Sonnenberg <tools@ludwigso.de>
2024-08-15 00:36:16 +0200

    Slightly refactor tests

---
+++ Application.Test/DomainFactory.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Model;
+
+namespace Application.Test;
+
+public static class DomainFactory
+{
+    public static Project NewProject(
+        ProjectSource source = ProjectSource.Hays,
+        string title = "Project 1",
+        string url = "http://example.com",
+        string? projectIdentifier = "123",
+        string? description = "Description",
+        string? jobLocation = "London"
+    )
+    {
+        return new Project(
+            source,
+            title,
+            url,
+            projectIdentifier,
+            description,
+            jobLocation
+        );
+    }
+}
