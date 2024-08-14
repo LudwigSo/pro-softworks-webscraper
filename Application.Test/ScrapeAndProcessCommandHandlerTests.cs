d025b9b79ace183bd7d14bed7036a851740fd5c1
Ludwig Sonnenberg <ludwig.sonnenberg@pro-softworks.com>
2024-08-15 00:36:16 +0200

    Slightly refactor tests

--- Application.Test/ScrapeAndProcessCommandHandlerTests.cs
+++ Application.Test/ScrapeAndProcessCommandHandlerTests.cs
@@ -1,30 +1,14 @@
 using Application.Ports;
 using Application.Webscraper;
 using Domain.Model;
+using Domain.Ports;
 using FakeItEasy;
 
 namespace Application.Test;
 
 public class ScrapeAndProcessCommandHandlerTests
 {
-    private static Project ProjectFactory(
-        ProjectSource source = ProjectSource.Hays,
-        string title = "Project 1",
-        string url = "http://example.com",
-        string? projectIdentifier = "123",
-        string? description = "Description",
-        string? jobLocation = "London"
-    )
-    {
-        return new Project(
-            source,
-            title,
-            url,
-            projectIdentifier,
-            description,
-            jobLocation
-        );
-    }
+    
 
     [Fact]
     public async Task Test1()
@@ -32,20 +16,19 @@ public class ScrapeAndProcessCommandHandlerTests
         // Arrange
         var projects = new List<Project>
         {
-            ProjectFactory(title: "Project 1", projectIdentifier: "123"),
-            ProjectFactory(title: "Project 2", projectIdentifier: "234"),
-            ProjectFactory(title: "Project 3", projectIdentifier: "345"),
+            DomainFactory.NewProject(title: "Project 1", projectIdentifier: "123"),
+            DomainFactory.NewProject(title: "Project 2", projectIdentifier: "234"),
+            DomainFactory.NewProject(title: "Project 3", projectIdentifier: "345"),
         };
+
+        var fakeLogger = A.Fake<ILogger>();
+        var fakeWriteContext = A.Fake<IWriteContext>();
+        var fakeReadContext = FakeFactory.NewReadContext();
 
         var fakeWebscraperPort = A.Fake<IWebscraperPort>();
         A.CallTo(() => fakeWebscraperPort.Scrape(ProjectSource.Hays)).Returns(projects);
 
-        var fakeReadContext = A.Fake<IReadContext>();
-        A.CallTo(() => fakeReadContext.Projects).Returns(new List<Project>().AsQueryable());
-
-        var fakeWriteContext = A.Fake<IWriteContext>();
-
-        var handler = new ScrapeAndProcessCommandHandler(fakeWebscraperPort, fakeReadContext, fakeWriteContext);
+        var handler = new ScrapeAndProcessCommandHandler(fakeWebscraperPort, fakeReadContext, fakeWriteContext, fakeLogger);
         var command = new ScrapeAndProcessCommand(ProjectSource.Hays);
 
         // Act
