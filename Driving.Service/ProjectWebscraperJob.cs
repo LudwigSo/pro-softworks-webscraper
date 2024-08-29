f3f3e13c6d3d82b3d9770e72d4cd8879694fcdac
Ludwig Sonnenberg <ludwig.sonnenberg@pro-softworks.com>
2024-08-29 21:24:28 +0200

    Add Driving.Service project

---
+++ Driving.Service/ProjectWebscraperJob.cs
@@ -0,0 +1,24 @@
+﻿using Application.Webscraper;
+using Domain.Model;
+using Quartz;
+
+namespace Driving.Service;
+
+internal class HaysWebscraperJob(ScrapeAndProcessCommandHandler scrapeAndProcessCommandHandler)
+    : ProjectWebscraperJob(scrapeAndProcessCommandHandler)
+{
+    protected override ProjectSource Source { get; } = ProjectSource.Hays;
+}
+
+internal abstract class ProjectWebscraperJob(ScrapeAndProcessCommandHandler scrapeAndProcessCommandHandler)
+    : IJob
+{
+    protected abstract ProjectSource Source { get; }
+    private ScrapeAndProcessCommandHandler ScrapeAndProcessCommandHandler { get; } = scrapeAndProcessCommandHandler ?? throw new ArgumentNullException(nameof(scrapeAndProcessCommandHandler));
+
+    public async Task Execute(IJobExecutionContext context)
+    {
+        var command = new ScrapeAndProcessCommand(Source);
+        await ScrapeAndProcessCommandHandler.Handle(command);
+    }
+}
