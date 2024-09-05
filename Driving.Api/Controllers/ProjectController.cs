2e374ede8a3dde8d3d79a2c2dfbdfe1be7e5f162
Ludwig Sonnenberg <tools@ludwigso.de>
2024-09-06 01:00:35 +0200

    Add Retag command

--- Driving.Api/Controllers/ProjectController.cs
+++ Driving.Api/Controllers/ProjectController.cs
@@ -1,3 +1,4 @@
+using Domain.CommandHandlers;
 using Domain.Model;
 using Domain.Ports;
 using Microsoft.AspNetCore.Mvc;
@@ -6,19 +7,23 @@ namespace Driving.Api.Controllers
 {
     [ApiController]
     [Route("[controller]/[action]")]
-    public class ProjectController : ControllerBase
+    public class ProjectController(
+        IProjectQueriesPort projectQueries, 
+        ReTagCommandHandler reTagCommandHandler
+    ) : ControllerBase
     {
-        private readonly IProjectQueriesPort _projectQueries;
-
-        public ProjectController(IProjectQueriesPort projectQueries)
-        {
-            _projectQueries = projectQueries ?? throw new ArgumentNullException(nameof(projectQueries));
-        }
-
+        private readonly IProjectQueriesPort _projectQueries = projectQueries ?? throw new ArgumentNullException(nameof(projectQueries));
+        private readonly ReTagCommandHandler _reTagCommandHandler = reTagCommandHandler ?? throw new ArgumentNullException(nameof(reTagCommandHandler));
 
         [HttpGet]
         [ProducesResponseType(typeof(IEnumerable<Project>), StatusCodes.Status200OK)]
         public async Task<IEnumerable<Project>> AllActive() 
-            => await _projectQueries.GetActive(); 
+            => await _projectQueries.GetActive();
+
+        [HttpPost]
+        [ProducesResponseType(typeof(void), StatusCodes.Status200OK)]
+        public async Task Retag()
+            => await _reTagCommandHandler.Handle(new ReTagCommand());
+
     }
 }
