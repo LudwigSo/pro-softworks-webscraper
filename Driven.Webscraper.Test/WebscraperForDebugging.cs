5313aec9ad0f615430c1d0394c9e642c414b555b
Ludwig Sonnenberg <ludwig.sonnenberg@pro-softworks.com>
2024-09-10 14:10:13 +0200

    Add Debugging project for Webscraper

---
+++ Driven.Webscraper.Test/WebscraperForDebugging.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Model;
+using Domain.Ports;
+
+namespace Driven.Webscraper.Test
+{
+    public class WebscraperForDebugging : IWebscraperPort
+    {
+        public async Task<List<Project>> Scrape(ProjectSource source)
+        {
+            return new List<Project>
+            {
+                new Project(source, "C# Senior Dev", "www.example.org", "10", ExampleDescription(), null),
+                new Project(source, "C# Dev", "www.example.org", "11", null, null),
+                new Project(source, ".NET Senior Dev", "www.example.org", "12", ExampleDescription(), null),
+                new Project(source, "Typecript Senior Dev", "www.example.org", "13", null, null),
+                new Project(source, "DDD", "www.example.org", "14", null, null),
+                new Project(source, "Vue.js", "www.example.org", "15", ExampleDescription(), null),
+                new Project(source, "domain driven design", "www.example.org", "16", null, null),
+            };
+        }
+
+        private string ExampleDescription()
+        {
+            return "lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum";
+        }
+    }
+}
