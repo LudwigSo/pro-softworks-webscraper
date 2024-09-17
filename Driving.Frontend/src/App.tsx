47e91558e8ad3df8a824adbe445f63a5d9c381cb
bparungo <bparungo@web.de>
2024-09-17 17:14:06 +0200

    push currentstate

--- Driving.Frontend/src/App.tsx
+++ Driving.Frontend/src/App.tsx
@@ -1,9 +1,19 @@
 import { useEffect, useState } from "react";
-import "./App.css";
+import "./index.css";
 import { Button } from "./components/ui/button";
 import { tagApi } from "./api-configs";
 import { Tag } from "./api";
 import { Input } from "./components/ui/input";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { ThemeProvider } from "./components/custom/theme-provider";
+import { ModeToggle } from "./components/custom/mode-toggle";
+import ProjectHeader from "./components/custom/project-header";
+import { Separator } from "./components/ui/separator";
+import { Badge } from "./components/ui/badge";
 
 function App() {
   const [data, setData] = useState<Tag[] | null>(null);
@@ -37,27 +47,64 @@ function App() {
 
   return (
     <>
-      <div>
-        <div onClick={() => fetchData()}>
-          {data ? (
-            <pre>{JSON.stringify(data, null, 2)}</pre>
-          ) : loading ? (
-            "Loading..."
-          ) : (
-            "Fetch Data"
-          )}
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <ModeToggle />
+        <div>
+          <ResizablePanelGroup
+            direction="horizontal"
+            className="h-100 max-h-screen"
+          >
+            <ResizablePanel defaultSize={20}>
+              <ResizablePanelGroup direction="vertical">
+                <ResizablePanel defaultSize={25}>
+                  <ProjectHeader />
+                  <Separator />
+                  <p className="pl-6 pt-6">Add Tags</p>
+                  <div className="flex items-center justify-center p-4 margin auto">
+                    {inputValue}
+                    <Input
+                      value={inputValue}
+                      onInput={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        setInputValue(e.target.value)
+                      }
+                    />
+                    <Button onClick={addTag}>Add Tag</Button>
+                  </div>
+                </ResizablePanel>
+                <ResizableHandle withHandle />
+                <ResizablePanel defaultSize={25}>
+                  <p className="pl-6 pt-6">All Tags</p>
+                  <div className="flex p-8">
+                    {data ? (
+                      <div className="flex flex-wrap gap-2">
+                        {data.map((tag) => (
+                          <Badge key={tag.id}>{tag.name}</Badge>
+                        ))}
+                      </div>
+                    ) : loading ? (
+                      "Loading..."
+                    ) : (
+                      "Fetch Data"
+                    )}
+                  </div>
+                </ResizablePanel>
+              </ResizablePanelGroup>
+            </ResizablePanel>
+            <ResizableHandle withHandle />
+            <ResizablePanel defaultSize={80} className="!overflow-auto">
+              <div onClick={() => fetchData()}>
+                {data ? (
+                  <pre>{JSON.stringify(data, null, 2)}</pre>
+                ) : loading ? (
+                  "Loading..."
+                ) : (
+                  "Fetch Data"
+                )}
+              </div>
+            </ResizablePanel>
+          </ResizablePanelGroup>
         </div>
-      </div>
-      <div className="flex w-full max-w-sm items-center space-x-2">
-        {inputValue}
-        <Input
-          value={inputValue}
-          onInput={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setInputValue(e.target.value)
-          }
-        />
-        <Button onClick={addTag}>add tag</Button>
-      </div>
+      </ThemeProvider>
     </>
   );
 }
---
+++ Driving.Frontend/src/components/custom/project-header.tsx
@@ -0,0 +1,11 @@
+const ProjectHeader = () => {
+  return (
+    // JSX markup for your component goes here
+    <div className="relative pl-4 pb-4">
+      <h2 className="pl-11 pt-8 ">roject Viewer</h2>
+      <img src="favicon.svg" className="w-[50px] absolute top-[10px]" />
+    </div>
+  );
+};
+
+export default ProjectHeader;
