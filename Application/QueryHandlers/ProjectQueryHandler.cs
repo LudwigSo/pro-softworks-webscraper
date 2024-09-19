dae714afa16b44695587319ca587829216a03f88
Ludwig Sonnenberg <tools@ludwigso.de>
2024-09-19 21:40:24 +0200

    Fix query handler

--- Application/QueryHandlers/ProjectQueryHandler.cs
+++ Application/QueryHandlers/ProjectQueryHandler.cs
@@ -1,19 +1,17 @@
 ﻿using Domain;
 using Driven.Persistence.Postgres;
 using Microsoft.EntityFrameworkCore;
-using System.Reflection.Metadata;
 
-namespace Application.QueryHandlers
+namespace Application.QueryHandlers;
+
+public record ProjectsWithAnyTagQuery(DateTime Since);
+
+public class ProjectQueryHandler(Context context)
 {
-    public record ProjectsWithAnyTagQuery(DateTime Since);
+    private readonly Context _context = context ?? throw new ArgumentNullException(nameof(context));
 
-    public class ProjectQueryHandler(Context context)
+    public Task<Project[]> Handle(ProjectsWithAnyTagQuery query)
     {
-        private readonly Context _context = context ?? throw new ArgumentNullException(nameof(context));
-
-        public Task<Project[]> Handle(ProjectsWithAnyTagQuery query)
-        {
-            return _context.Projects.Include(p => p.Tags).Where(x => x.FirstSeenAt >= query.Since && x.Tags.Count != 0).ToArrayAsync();
-        }
+        return _context.Projects.Include(p => p.Tags).Where(x => x.FirstSeenAt >= query.Since && x.Tags.Count != 0).ToArrayAsync();
     }
 }
