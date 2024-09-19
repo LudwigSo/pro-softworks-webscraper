2bf105342f365d991e37f4e797c3456194a028c3
Ludwig Sonnenberg <tools@ludwigso.de>
2024-09-19 10:18:52 +0200

    Minor cleanup

--- Domain/Ports/IRealtimeMessagesPort.cs
+++ Domain/Ports/IRealtimeMessagesPort.cs
@@ -5,5 +5,4 @@ namespace Domain.Ports;
 public interface IRealtimeMessagesPort
 {
     Task NewProjectsAdded(IEnumerable<Project> projects);
-    Task ProjectsRemoved(IEnumerable<Project> projects);
 }
