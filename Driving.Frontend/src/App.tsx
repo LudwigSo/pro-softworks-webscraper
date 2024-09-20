0d378140709f7933172f9b062d9986cfc3c62d6e
bparungo <bparungo@web.de>
2024-09-20 17:30:55 +0200

    add project section

--- Driving.Frontend/src/App.tsx
+++ Driving.Frontend/src/App.tsx
@@ -1,7 +1,4 @@
-import { useEffect, useState } from "react";
 import "./index.css";
-import { tagApi } from "./api-configs";
-import { Tag } from "./api";
 import {
   ResizableHandle,
   ResizablePanel,
@@ -12,60 +9,44 @@ import { ModeToggle } from "./components/custom/mode-toggle";
 import ProjectHeader from "./components/custom/project-header";
 import { Separator } from "./components/ui/separator";
 import { Toaster } from "./components/ui/toaster";
-import { errorToast } from "./supplements/toasts";
 import ManageTags from "./components/custom/manage-tags";
+import ProjectsContainer from "./components/custom/projects-container";
+import React, { useState } from "react";
+import { Project } from "./api";
+
+type ProjectState = [
+  Project[],
+  React.Dispatch<React.SetStateAction<Project[]>>,
+];
+
+export const Context = React.createContext<ProjectState>([[], () => {}]);
 
 function App() {
-  const [data, setData] = useState<Tag[] | null>(null);
-  const [loading, setLoading] = useState(true);
-
-  async function fetchData() {
-    setLoading(true);
-    setData(null);
-    try {
-      const tags = await tagApi.tagAllGet();
-      setData(tags.data);
-    } catch (error) {
-      errorToast(error);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
+  const [projects, setProjects]: ProjectState = useState<Project[]>([]);
   return (
     <>
-      <Toaster />
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <ModeToggle />
-        <div>
-          <ResizablePanelGroup
-            direction="horizontal"
-            className="h-100 min-h-screen max-h-screen"
-          >
-            <ResizablePanel defaultSize={20}>
-              <ProjectHeader />
-              <Separator />
-              <ManageTags />
-            </ResizablePanel>
-            <ResizableHandle withHandle />
-            <ResizablePanel defaultSize={80} className="!overflow-auto">
-              <div onClick={() => fetchData()}>
-                {data ? (
-                  <pre>{JSON.stringify(data, null, 2)}</pre>
-                ) : loading ? (
-                  "Loading..."
-                ) : (
-                  "Fetch Data"
-                )}
-              </div>
-            </ResizablePanel>
-          </ResizablePanelGroup>
-        </div>
-      </ThemeProvider>
+      <Context.Provider value={[projects, setProjects]}>
+        <Toaster />
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          <ModeToggle />
+          <div>
+            <ResizablePanelGroup
+              direction="horizontal"
+              className="h-100 min-h-screen max-h-screen"
+            >
+              <ResizablePanel defaultSize={20}>
+                <ProjectHeader />
+                <Separator />
+                <ManageTags />
+              </ResizablePanel>
+              <ResizableHandle withHandle />
+              <ResizablePanel defaultSize={80}>
+                <ProjectsContainer />
+              </ResizablePanel>
+            </ResizablePanelGroup>
+          </div>
+        </ThemeProvider>
+      </Context.Provider>
     </>
   );
 }
