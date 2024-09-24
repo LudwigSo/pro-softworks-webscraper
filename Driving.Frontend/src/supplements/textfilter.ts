bccd3e8d4f73fc7c953c2a476bce31207eee77e8
bparungo <bparungo@web.de>
2024-09-24 13:47:38 +0200

    add regexbased search

--- Driving.Frontend/src/supplements/textfilter.ts
+++ Driving.Frontend/src/supplements/textfilter.ts
@@ -1,10 +1,20 @@
 export function textFilter(object: object, text: string): boolean {
+  let regex: RegExp;
+  try {
+    regex = new RegExp(text, "i");
+  } catch {
+    return false;
+  }
+
   return Object.values(object).some((value) => {
     if (typeof value === "string") {
-      return value.toLowerCase().includes(text.toLowerCase());
+      return regex.test(value);
     }
     if (Array.isArray(value)) {
       return value.some((v) => textFilter(v, text));
+    }
+    if (typeof value === "object" && value !== null) {
+      return textFilter(value, text);
     }
     return false;
   });
